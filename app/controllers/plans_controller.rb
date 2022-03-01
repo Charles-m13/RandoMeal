c8eca30c78b8cae84c1d8586d6d78049278ff861
Pierre LEMASSON <sir@chamallow.com>
2022-03-01 18:02:42 +0100

    Add plan controlller + navbar + MAJ routes.rb + MAJ _navbar.scss

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,2 +1,30 @@
 class PlansController < ApplicationController
+
+  def index
+    @plans = Plan.all
+  end
+
+  def show
+    @plan = Plan.find(params[:id])
+  end
+
+  def save
+  end
+
+  def create
+    @plan = Plan.new(plan_params)
+    @plan.save
+
+    redirect_to plan_path(@plan)
+  end
+
+  private
+
+  def plan_params
+    params.require(:plan).permit(:name)
+    params.require(:recipe).permit(:name, :image, :url_marmiton, :price)
+    params.require(:ingredient).permit(:name, :quantity, :mesurement)
+    params.require(:tag).permit(:name)
+  end
+
 end
