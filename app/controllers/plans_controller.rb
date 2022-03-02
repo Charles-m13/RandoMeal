2607094b8796fbf3e2f6d276490147092cb4088b
Pierre LEMASSON <sir@chamallow.com>
2022-03-02 14:00:43 +0100

    MAJ : Plan controlller + Page Planificateur + navbar + routes.rb + _navbar.scss

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,30 +1,38 @@
 class PlansController < ApplicationController
 
   def index
-    @plans = Plan.all
+    # @plans = Plan.all
+  end
+
+  def new
+  end
+
+  def create
+    # @plan = Plan.new(plan_params)
+    # @plan.save
+    # redirect_to plan_path(@plan)
   end
 
   def show
-    @plan = Plan.find(params[:id])
+    # @plan = Plan.find(params[:id])
   end
 
   def save
   end
 
-  def create
-    @plan = Plan.new(plan_params)
-    @plan.save
+  def edit
+  end
 
-    redirect_to plan_path(@plan)
+  def destroy
   end
 
   private
 
   def plan_params
-    params.require(:plan).permit(:name)
-    params.require(:recipe).permit(:name, :image, :url_marmiton, :price)
-    params.require(:ingredient).permit(:name, :quantity, :mesurement)
-    params.require(:tag).permit(:name)
+    # params.require(:plan).permit(:name)
+    # params.require(:recipe).permit(:name, :image, :url_marmiton, :price, :prep_duration, :total_duration, :people)
+    # params.require(:ingredient).permit(:name, :quantity, :mesurement, :recipe_id)
+    # params.require(:tag).permit(:name, :marmiton_filter)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'plans#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "plans", to: "plans#index"
-  get "plans/:id", to: "plans#show" 
+  get    "plans",          to: "plans#index"
+  get    "plans/new",      to: "plans#new"
+  post   "plans",          to: "plans#create"
+  # NB: The `show` route needs to be *after* `new` route.
+  get    "plans/:id",      to: "plans#show"
+  get    "plans/:id/edit", to: "plans#edit"
+  patch  "plans/:id",      to: "plans#update"
+  delete "plans/:id",      to: "plans#destroy"
+
+  resources :plans, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+    resources :ingredients, only: [:index, :show]
+    resources :recipes, only: [:index, :show]
+    resources :recipe_tags, only: [:index, :show]
+    resources :tags, only: [:index, :show]
+  end
 end
