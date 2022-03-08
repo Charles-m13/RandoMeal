215728538cd9d802b6b9f4f88771cba5241f337b
Deborah MARTIN <deb.martin@live.fr>
2022-03-08 09:35:16 +0100

    Created js feature to randomize and render new recipes

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -2,29 +2,44 @@ class PlansController < ApplicationController
 
   def index
     @recipes = Recipe.order('RANDOM()').limit(5)
+    cookies[:plan_recipes] = []
+    cookies[:already_proposed] = @recipes.map(&:id).map(&:to_s).join(',')
   end
 
-  def new
+  def add
+    ap cookies[:plan_recipes]
+    @recipe = Recipe.find(params[:recipe_id])
+    plan_recipes = cookies[:plan_recipes].split(',')
+    plan_recipes << @recipe.id.to_s
+    ap Recipe.where(id: plan_recipes).pluck(:name)
+    cookies[:plan_recipes] = plan_recipes.join(',')
   end
 
-  def create
-    # @plan = Plan.new(plan_params)
-    # @plan.save
-    # redirect_to plan_path(@plan)
+  def remove
+    @recipe = Recipe.find(params[:recipe_id])
+    plan_recipes = cookies[:plan_recipes].split(',')
+    plan_recipes = plan_recipes - [@recipe.id.to_s]
+    ap Recipe.where(id: plan_recipes).pluck(:name)
+    cookies[:plan_recipes] = plan_recipes.join(',')
   end
 
-  def show
-    # @plan = Plan.find(params[:id])
+  def refresh
+    plan_recipes = cookies[:plan_recipes].split(',')
+    already_proposed = cookies[:already_proposed].split(',')
+    nb = 5 - plan_recipes.length
+
+    new_recipes = RandomRecipes.new(already_proposed, nb).call
+    already_proposed += new_recipes.map(&:id)
+    cookies[:already_proposed] = already_proposed.map(&:to_s).join(',')
+
+    recipe_cards = new_recipes.map do |recipe|
+      render_to_string(partial: "shared/card", locals: { recipe: recipe })
+    end
+
+    data = {recipe_cards: recipe_cards}
+    render json: data
   end
 
-  def save
-  end
-
-  def edit
-  end
-
-  def destroy
-  end
 
   private
 
