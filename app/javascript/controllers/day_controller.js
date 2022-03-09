fc21addd5d2026af941e77e698d7555b91ecceb1
Pierre LEMASSON <sir@chamallow.com>
2022-03-09 15:03:06 +0100

    blocage bouton + et - si bouton block est actif & MAJ layout PDF.html.erb : calcul Nombre de Personnes et quantité + liste de courses

--- app/javascript/controllers/day_controller.js
+++ app/javascript/controllers/day_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ['day', 'nbPerson']
+  static targets = ['day', 'nbPerson','increase','decrease']
   static values = {
     recipeId: Number
   }
@@ -13,16 +13,19 @@ export default class extends Controller {
   toggleLock() {
     this.locked = !this.locked
     this.dayTarget.classList.toggle('locked', this.locked)
+    this.increaseTarget.removeAttribute('data-action')
+    this.decreaseTarget.removeAttribute('data-action')
     const nbPerson = this.nbPersonTarget.innerHTML
     const csrfToken = document.querySelector("[name='csrf-token']").content;
     const method = this.locked ? 'add' : 'remove'
     const url = `/plans/${method}?recipe_id=${this.recipeIdValue}&nb_person=${nbPerson}`
-
+    
     fetch(url, {
-     method: 'POST',
-     headers:  {
+      method: 'POST',
+      headers:  {
         "X-CSRF-Token": csrfToken
-     }
-   })
+      }
+
+    })
   }
 }
